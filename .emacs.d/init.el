2268389326c6984ca13078ee239b24f6a3338669
Jung <myjung@ea.com>
2021-02-04 09:49:51 +0900

    setting
    - set xml mode indent to 4
    - add exports option for confluence wiki export at org babel plantuml
    - add ini-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,9 +20,10 @@
    '("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "ae65ccecdcc9eb29ec29172e1bfb6cadbe68108e1c0334f3ae52414097c501d2" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" default))
  '(helm-gtags-prefix-key "tg")
  '(helm-gtags-suggested-key-mapping t)
+ '(nxml-child-indent 4)
  '(org-agenda-files '("~/work/todo.org"))
  '(package-selected-packages
-   '(ox-confluence-en browse-kill-ring eshell-toggle google-translate lua-mode org-download undo-tree yasnippet projectile flycheck company wgrep ivy p4 emacs-surround true highlight-indent-guides material-theme spacemacs-theme helpful ns-auto-titlebar json-mode actionscript-mode quelpa-use-package helm counsel ivy-xref org-plus-contrib lsp-ivy undo-tree-mode shader-mode edit-indirect flycheck-iron swiper powerline key-chord expand-region iy-go-to-char ccls dap-mode treemacs lsp-treemacs lsp-ui lsp-mode ggtags autopair python-black jedi powershell yasnippet-snippets yaml-mode wsd-mode which-key wgrep-ag w3 use-package-chords solarized-theme rtags rg python-docstring pyenv-mode projectile-ripgrep prodigy plantuml-mode org-projectile omnisharp narrowed-page-navigation narrow-reindent multishell mark-multiple magit jupyter irony-eldoc iedit ibuffer-projectile google-c-style flymake-yaml flymake-shell flymake-lua flymake-json flycheck-pycheckers flycheck-irony eyebrowse exec-path-from-shell evil elpy el-get ein edit-server dotnet company-shell company-lua company-jedi company-irony-c-headers company-irony company-glsl company-anaconda command-log-mode cmake-mode cmake-ide blacken avy airline-themes ace-jump-mode)))
+   '(plantuml-mode reveal-in-folder ini-mode ox-confluence-en browse-kill-ring eshell-toggle google-translate lua-mode org-download undo-tree yasnippet projectile flycheck company wgrep ivy p4 emacs-surround true highlight-indent-guides material-theme spacemacs-theme helpful ns-auto-titlebar json-mode actionscript-mode quelpa-use-package helm counsel ivy-xref org-plus-contrib lsp-ivy undo-tree-mode shader-mode edit-indirect flycheck-iron swiper powerline key-chord expand-region iy-go-to-char ccls dap-mode treemacs lsp-treemacs lsp-ui lsp-mode ggtags autopair python-black jedi powershell yasnippet-snippets yaml-mode wsd-mode which-key wgrep-ag w3 use-package-chords solarized-theme rtags rg python-docstring pyenv-mode projectile-ripgrep prodigy org-projectile omnisharp narrowed-page-navigation narrow-reindent multishell mark-multiple magit jupyter irony-eldoc iedit ibuffer-projectile google-c-style flymake-yaml flymake-shell flymake-lua flymake-json flycheck-pycheckers flycheck-irony eyebrowse exec-path-from-shell evil elpy el-get ein edit-server dotnet company-shell company-lua company-jedi company-irony-c-headers company-irony company-glsl company-anaconda command-log-mode cmake-mode cmake-ide blacken avy airline-themes ace-jump-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -280,7 +281,7 @@
 	      (when org-inline-image-overlays
 		(org-redisplay-inline-images))))
   (add-to-list 'org-structure-template-alist
-	       '("u" . "src plantuml :file .png"))
+	       '("u" . "src plantuml :file .png :exports plantuml"))
   (setq org-startup-with-inline-images t)
   ;; (add-hook 'org-mode-hook 'org-indent-mode)
   (global-set-key (kbd "C-c o a") 'org-agenda)
@@ -537,5 +538,10 @@ same directory as the org-buffer and insert a link to this file."
   (setq edit-server-url-major-mode-alist
         '(("github\\.com" . markdown-mode)))
   (edit-server-start))
+
+(use-package ini-mode
+  :ensure t
+  :config
+  (setq ))
 
 (server-start)
