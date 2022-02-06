62ca4ea36d885e1b20a9714c9dfdff47d3fd0825
Myonghwa, Jung <kino811@gmail.com>
2022-02-06 17:37:13 +0900

    set spacemacs theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,15 +1,1 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/myInit.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(expand-region magit rg zenburn-theme which-key use-package try spacemacs-theme solarized-theme org-bullets counsel color-theme auto-complete ace-window)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
